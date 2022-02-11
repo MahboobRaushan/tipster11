6d515c1ffee29cc159273c646bac2ab9a66825c3
MahboobRaushan <mahboob.raushan@gmail.com>
2022-02-11 20:00:28 +0530

    11-02-2022 3rd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')->hourly();
          $schedule->command('pool:running')
-            ->everyMinute();
+            ->everyFiveMinutes();
     }
     
     /**
