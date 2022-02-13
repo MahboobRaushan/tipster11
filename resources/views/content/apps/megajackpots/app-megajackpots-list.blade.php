34b571f3fd8c42586ceff74da7472dc684856e71
MahboobRaushan <mahboob.raushan@gmail.com>
2022-02-13 09:18:56 +0530

    13-02-2022 1st

--- resources/views/content/apps/megajackpots/app-megajackpots-list.blade.php
+++ resources/views/content/apps/megajackpots/app-megajackpots-list.blade.php
@@ -10,12 +10,14 @@
   <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/buttons.bootstrap5.min.css')) }}">
   <link rel="stylesheet" href="{{ asset(mix('vendors/css/tables/datatable/rowGroup.bootstrap5.min.css')) }}">
 
-
+ <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/wizard/bs-stepper.min.css')) }}">
+  <link rel="stylesheet" href="{{ asset(mix('vendors/css/forms/spinner/jquery.bootstrap-touchspin.css'))}}">
 
 @endsection
 
 @section('page-style')
   {{-- Page Css files --}}
+  <link rel="stylesheet" href="{{ asset('css/base/pages/ui-feather.css') }}">
   <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
 @endsection
 
@@ -40,7 +42,7 @@
         <tbody>
           <tr>
             <td>Current Mega Jackpot</td>           
-            <td><strong>Big Jackpot</strong></td>  
+            <td><strong>1</strong></td>  
           </tr>
           <tr>
             <td>Start Time</td>           
@@ -48,19 +50,29 @@
           </tr>
           <tr>
             <td>End Time</td>           
-            <td><strong>2022-01-19 2.59 p.m.</strong></td>  
+            <td><strong>XXXX</strong></td>  
           </tr>
           <tr>
-            <td>Base Price</td>           
-            <td><strong>$2000</strong></td>  
+            <td>Base Prize</td>           
+            <td><div class="input-group">
+              <span>$</span><input
+              name="base_prize"
+              id="base_prize"
+                type="text"
+                class="touchspin-min-max"
+                value="2000"
+                data-bts-button-down-class="btn btn-warning"
+                data-bts-button-up-class="btn btn-success"
+              />
+            </div>  <button class="btn btn-info confirmitem"  data-bs-toggle="modal" data-bs-target="#myModal_directcredits">Confirm</button></td>  
           </tr>
           <tr>
-            <td>Accumulate Price</td>           
-            <td><strong>$8800</strong></td>  
+            <td>Accumulate Prize</td>           
+            <td><strong><span>$</span><span id="accumulate_prize">8800</span></strong></td>  
           </tr>
           <tr>
-            <td>Total Price</td>           
-            <td><strong>$10000</strong></td>  
+            <td>Current Mega Jackpot Prize</td>           
+            <td><strong><span>$</span><span id="total_prize">10000</span></strong></td>  
           </tr>
         </tbody>
       </table>
@@ -74,9 +86,12 @@
             <th>Name</th>
             <th>Start Time</th>           
             <th>End Time</th>
-            <th>Base Price</th>
-            <th>Accumulate Price</th>
-            <th>Total Price</th>
+            <th>Final Prize (Pool Final Total Prize)</th>
+            <th>Winner</th>
+            <th>Contributed Amount to Mega Jackpot</th>
+            <th>Which Mega Jackpot</th>
+            <th>Status</th>
+            <th>Actions</th>
             
           </tr>
         </thead>
@@ -102,9 +117,9 @@
             
             <th>Start Time</th>           
             <th>End Time</th>
-            <th>Base Price</th>
-            <th>Accumulate Price</th>
-            <th>Total Price</th>
+            <th>Base Prize</th>
+            <th>Accumulate Prize</th>
+            <th>Total Prize</th>
             <th>Winner</th>
             
           </tr>
@@ -117,6 +132,45 @@
   <!-- list and filter end -->
 </section>
 <!-- megajackpotss list ends -->
+
+ <div class="modal fade" id="myModal_directcredits" tabindex="-1" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header bg-transparent">
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body p-3 pt-0">
+          <form action="#" id="confirm_Form">
+          @csrf 
+            <div class="text-center mb-2">
+              <h1 class="mb-1">Base Prize</h1>
+               <div id="confirm_message_div" class="text-danger"></div>
+              <p>Are you sure?</p>
+            </div>
+
+            <div class="alert alert-warning" role="alert">
+              <h6 class="alert-heading">Warning!</h6>
+              <div class="alert-body">
+                Do you really want to this ? This process cannot be undone.
+              </div>
+            </div>
+
+           
+             
+            
+              <div class="col-sm-12 ps-sm-0">
+               
+                <button type="submit" id="btn_save_confirm" class="btn btn-warning data-delete">Submit</button>
+                 <button type="reset" class="btn btn-outline-secondary" data-bs-dismiss="modal">Cancel</button>
+              </div>
+            </form>
+             
+           
+          </div>
+        </div>
+      </div>
+    </div>
+
 @endsection
 
 @section('vendor-script')
@@ -129,7 +183,11 @@
 
 
   
- 
+    <script src="{{ asset(mix('vendors/js/forms/wizard/bs-stepper.min.js')) }}"></script>
+<script src="{{ asset(mix('vendors/js/pickers/flatpickr/flatpickr.min.js')) }}"></script>
+
+<script src="{{ asset(mix('vendors/js/forms/spinner/jquery.bootstrap-touchspin.js'))}}"></script>
+
 
 
 
@@ -141,10 +199,50 @@
 <script> 
 
 var custom_get_all_permissions_access_Array = <?php echo json_encode($custom_get_all_permissions_access); ?>; 
+
 </script>
 
 
   <script src="{{ asset(mix('js/scripts/pages/app-megajackpots-list.js')) }}"></script>
-  }
+<script src="{{ asset(mix('js/scripts/forms/form-number-input-zero-lacs.js'))}}"></script>
+<script>
+
+ $('.bootstrap-touchspin-down').on('click', function () {
+            
+            var base_prize = $('#base_prize').val();
+            base_prize = parseInt(base_prize);
+
+            
+            var accumulate_prize = $('#accumulate_prize').html();
+            accumulate_prize = parseInt(accumulate_prize);
+
+            var total_prize = base_prize+accumulate_prize;
+            $('#total_prize').html(total_prize);
+        });
+          $('.bootstrap-touchspin-up').on('click', function () {
+            
+             var base_prize = $('#base_prize').val();
+            base_prize = parseInt(base_prize);
+
+            
+            var accumulate_prize = $('#accumulate_prize').html();
+            accumulate_prize = parseInt(accumulate_prize);
+
+            var total_prize = base_prize+accumulate_prize;
+            $('#total_prize').html(total_prize);
+        });
+          $('#base_prize').on('keyup', function () {
+            var base_prize = $('#base_prize').val();
+            base_prize = parseInt(base_prize);
+
+            
+            var accumulate_prize = $('#accumulate_prize').html();
+            accumulate_prize = parseInt(accumulate_prize);
+
+            var total_prize = base_prize+accumulate_prize;
+            $('#total_prize').html(total_prize);
+        });
+
+</script>
 @endsection
 
